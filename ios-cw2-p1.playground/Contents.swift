b12b0311e94bc438a0d75d9ed42a50485bcefa51
ali3dashti <aadashti73@gmail.com>
2020-06-17 21:03:02 +0300

    تم حل الكلاس ورك

---
+++ ios-cw2-p1.playground/Contents.swift
@@ -0,0 +1,12 @@
+let family = ["adnan", "hussain","mohammed","ali","yousef"]
+
+var evens = [2, 4, 6, 8, 10, 12]
+
+var grades = [90.1, 89, 91.7]
+
+var trueFalse = [true , true, true, false]
+
+print(family[0], family[4])
+print(evens[0], evens [5])
+print(grades[0], grades[2])
+print(trueFalse[0], trueFalse[3])
